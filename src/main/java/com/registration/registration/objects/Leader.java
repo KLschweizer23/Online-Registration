309ee38b1eda9b74ef2452a2b65ce7852073c1a3
Ken Lloyd Billones <Schweizer23.ss@gmail.com>
2022-06-20 14:59:22 +0800

    Refactored all objects by inheriting to AbstractPerson class

--- src/main/java/com/registration/registration/objects/Leader.java
+++ src/main/java/com/registration/registration/objects/Leader.java
@@ -1,12 +1,6 @@
 package com.registration.registration.objects;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.springframework.lang.NonNull;
@@ -14,78 +8,10 @@ import org.springframework.lang.NonNull;
 
 @Entity
 @Table(name = "leaders")
-public class Leader extends AbstractPerons{
+public class Leader extends AbstractPerson{
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true, length = 255)
-    private String email;
-
-    @Column(nullable = false, length = 255)
-    private String password;
-
-    @Column(nullable = false, length = 255)
-    private String firstName;
-
-    @Column(nullable = false, length = 255)
-    private String lastName;
-
-    @NonNull
-    @OneToOne(cascade = CascadeType.ALL)
-    private Church church;
-
     @NonNull
     private boolean admin;
-
-    public Church getChurch() {
-        return this.church;
-    }
-
-    public void setChurch(Church church) {
-        this.church = church;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
 	public boolean isAdmin() {
 		return this.admin;
@@ -94,6 +20,5 @@ public class Leader extends AbstractPerons{
 	public void setAdmin(boolean admin) {
 		this.admin = admin;
 	}
-
 
 }
