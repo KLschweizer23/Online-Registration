85f061f4fd2cf3db53735ff4806672e138f761e5
Ken Lloyd Billones <Schweizer23.ss@gmail.com>
2022-06-20 12:36:47 +0800

    Participant date change to string, and trying out Unit Testing

--- src/test/java/com/registration/registration/RepositoryTest.java
+++ src/test/java/com/registration/registration/RepositoryTest.java
@@ -1,5 +1,11 @@
 package com.registration.registration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -7,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
 
+import com.registration.registration.objects.Church;
 import com.registration.registration.objects.Participant;
 import com.registration.registration.repositories.ChurchRepository;
 import com.registration.registration.repositories.LeaderRepository;
@@ -29,6 +36,29 @@ public class RepositoryTest {
     @Autowired
     private TestEntityManager entityManager;
 
+    @Test
+    public void testCreateChurch(){
+        Church church = new Church();
+        church.setName("Living Hope Foursquare Gospel Church");
+        church.setAddress("Prk. Papaya, Mankilam, Tagum City");
 
+        Church savedChurch = churchRepo.save(church);
+
+        Church existChurch = entityManager.find(Church.class, savedChurch.getId());
+
+        assertThat(existChurch.getName()).isEqualTo(church.getName());
+    }
+
+    @Test
+    public void testCreateParticipant(){
+        Participant participant = new Participant();
+        participant.setEmail("Hyacinth@gmail.com");
+        participant.setPassword("klhicH100");
+        participant.setFirstName("Hyacinth");
+        participant.setLastName("Billones");
+        participant.setBirthday(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        participant.setPaid(false);
+        participant.setPlayer(false);
+    }
 
 }
