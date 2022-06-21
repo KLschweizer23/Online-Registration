5ebb46261482199ece6434e97489b59a83978441
Ken Lloyd Billones <Schweizer23.ss@gmail.com>
2022-06-21 21:24:51 +0800

    Added Sport Object

---
+++ src/main/java/com/registration/registration/objects/Sport.java
@@ -0,0 +1,61 @@
+package com.registration.registration.objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "sports")
+public class Sport {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+    
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    @NonNull
+    private int players;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPlayers() {
+        return this.players;
+    }
+
+    public void setPlayers(int players) {
+        this.players = players;
+    }
+
+}
