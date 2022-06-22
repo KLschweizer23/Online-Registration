ac0f9800e91546611eaefdf4c3ce9541fc785d8b
Ken Lloyd Billones <Schweizer23.ss@gmail.com>
2022-06-22 15:04:17 +0800

    Configured WebSecurityConfig

--- src/main/java/com/registration/registration/securities/WebSecurityConfig.java
+++ src/main/java/com/registration/registration/securities/WebSecurityConfig.java
@@ -1,5 +1,61 @@
 package com.registration.registration.securities;
 
+import javax.activation.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.registration.registration.details.service.CustomParticipantDetailsService;
+
 public class WebSecurityConfig{
     
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomParticipantDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationProvider uAuthenticationProvider(){
+        return authenticationProvider();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+            .antMatchers("/camper").authenticated()
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+                .usernameParameter("email")
+                .defaultSuccessUrl("/cmper")
+                .permitAll()
+            .and()
+            .logout().logoutSuccessUrl("/").permitAll();
+        
+        return http.build();
+    }
+
 }
