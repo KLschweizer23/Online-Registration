0b92aa9b60545bfb66b65e0d8cfc693056abbdf2
Ken Lloyd Billones <Schweizer23.ss@gmail.com>
2022-06-24 15:56:11 +0800

    Added counter object for table number

---
+++ src/main/java/com/registration/registration/objects/Counter.java
@@ -0,0 +1,10 @@
+package com.registration.registration.objects;
+
+public class Counter {
+    private int count = 0;
+
+    public int getCount() {
+        count++;
+        return count;
+    }
+}
