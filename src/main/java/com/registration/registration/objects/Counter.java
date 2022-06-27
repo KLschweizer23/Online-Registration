98fb5a8ee42501845792fbe643ad3fd7a8924bb1
Ken Lloyd Billones <Schweizer23.ss@gmail.com>
2022-06-27 14:35:49 +0800

    Added new functions to getcount in different variables

--- src/main/java/com/registration/registration/objects/Counter.java
+++ src/main/java/com/registration/registration/objects/Counter.java
@@ -1,14 +1,22 @@
 package com.registration.registration.objects;
 
 public class Counter {
-    private int count = 0;
+    private int count1 = 0;
+    private int count2 = 0;
+    private int count3 = 0;
 
-    public int getCount() {
-        count++;
-        return count;
+    public int getCount1() {
+        count1++;
+        return count1;
     }
 
-    public void zeroCount(){
-        count = 0;
+    public int getCount2() {
+        count2++;
+        return count2;
+    }
+
+    public int getCount3() {
+        count3++;
+        return count3;
     }
 }
